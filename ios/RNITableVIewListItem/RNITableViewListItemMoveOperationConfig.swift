91c3f5491d5d0d798d496baee56ed006882756fd
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-04-16 08:11:00 +0800

    ⭐️ Impl: `RNITableViewListItemMoveOperationConfig.applyConfig`

--- ios/RNITableVIewListItem/RNITableViewListItemMoveOperationConfig.swift
+++ ios/RNITableVIewListItem/RNITableViewListItemMoveOperationConfig.swift
@@ -19,6 +19,44 @@ public struct RNITableViewListItemMoveOperationConfig {
   public var destinationConfig: RNITableViewListItemTargetPositionConfig;
   
   public var shouldAnimateDifference: Bool;
+  public var shouldMoveItemAfterDestination: Bool;
+  
+  public func applyConfig(
+    toSnapshot snapshot: inout RNITableViewDataSourceSnapshot,
+    withReactListItems reactListItems: [RNITableViewListItem]
+  ) throws {
+    
+    let sourceItemID = try? self.sourceConfig.getMatchingItemIdentifier(
+      inSnapshot: snapshot,
+      withReactListItems: reactListItems
+    );
+    
+    guard let sourceItemID = sourceItemID else {
+      throw RNIListViewError(
+        errorCode: .unexpectedNilValue,
+        description: "Unable to get sourceItemID"
+      );
+    };
+    
+    let destinationID = try? sourceConfig.getMatchingItemIdentifier(
+      inSnapshot: snapshot,
+      withReactListItems: reactListItems
+    );
+    
+    guard let destinationID = destinationID else {
+      throw RNIListViewError(
+        errorCode: .unexpectedNilValue,
+        description: "Unable to get destinationID"
+      );
+    };
+    
+    if self.shouldMoveItemAfterDestination {
+      snapshot.moveItem(sourceItemID, afterItem: destinationID);
+      
+    } else {
+      snapshot.moveItem(sourceItemID, afterItem: destinationID);
+    };
+  };
 };
 
 extension RNITableViewListItemMoveOperationConfig: InitializableFromDictionary {
