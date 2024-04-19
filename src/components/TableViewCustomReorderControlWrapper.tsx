a0639e954ab159dab6c7cc84bc9fde2ae4e2d166
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-04-20 06:05:33 +0800

    🐞 Fix: `TableViewCustomReorderControlWrapper`

--- src/components/TableViewCustomReorderControlWrapper.tsx
+++ src/components/TableViewCustomReorderControlWrapper.tsx
@@ -6,7 +6,7 @@ import { TABLE_VIEW_NATIVE_ID_KEYS } from './TableView/Constants';
 export function TableViewCustomReorderControlWrapper(props: {
   children: JSX.Element;
 }) {
-  React.cloneElement(props.children as React.ReactElement<ViewProps>, {
+  return React.cloneElement(props.children as React.ReactElement<ViewProps>, {
     nativeID: TABLE_VIEW_NATIVE_ID_KEYS.customReorderControl,
   });
 };
